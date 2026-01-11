2243ed910c8c3728756ffc82c0c5449d7d3e6d4b
Rishu Raj <rrdebug7@gmail.com>
2026-01-11 18:28:01 +0530

    updated recent code

--- Weather app/Readme.md
+++ Weather app/Readme.md
@@ -1,1 +1,1 @@
-Just a Basic Weather app where I applied concepts like API calling etc
+Just a Basic Weather app where I applied concepts like API calling etc.
--- Weather app/scripts.js
+++ Weather app/scripts.js
@@ -6,29 +6,46 @@ const cityName= document.getElementById('city-name')
 const tempuratureDisplay  = document.getElementById('temperature')
 const descriptionDisplay = document.getElementById('description')
 const errorMessage = document.getElementById('error-message')
-const API_KEY = '3300f9f63c9a2fe7fa9090e0680719af'
+const API_KEY = "3300f9f63c9a2fe7fa9090e0680719af"
 getWeatherBtn.addEventListener('click',async()=>{
   const city =cityInput.value.trim()
   if (!city)return;
   try{
  const weatherData=  await fetchWeatherData(city)
+ displayWeatherData(weatherData)
   }catch(error){
-    showError()
+    showError();
   }
 })
 
 async function fetchWeatherData(city){
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
 
+  const response= await fetch(url)
+  console.log("RESPONSE", response)
+  console.log(typeof response)
+  if(!response.ok){
+    throw new Error("City not found") 
+  }
+  const data = await response.json()
+  return data  
 }
-function displayWeatherData(weatherData){
-
-}
-function showError(){
-
-}
-function showError(){
+function displayWeatherData(data){
+ console.log(data)
+ const{name,main,weather}= data 
+ cityName.textContent = name
+ tempuratureDisplay.textContent= `Tempurature -> ${main.temp}`
+ descriptionDisplay.textContent=`Weather -> ${weather[0].description}`
   weatherInfo.classList.add('hidden')
   weatherInfo.classList.remove('hidden')
+
+}
+function showError(){
+
+}
+function showError(){
+  weatherInfo.classList.remove('hidden')
+  weatherInfo.classList.add('hidden')
 }
 
   
